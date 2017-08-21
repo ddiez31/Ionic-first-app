a55ff35a5bc6c945917d44809dec29621246dc4b
ddiez31 <david.diez31@free.fr>
2017-08-21 16:51:54 +0200

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HttpModule } from '@angular/http';
-import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {HttpModule} from '@angular/http';
+import {CloudSettings, CloudModule} from '@ionic/cloud-angular';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { DetailsPage } from '../pages/details/details';
-import { MeteoPage } from '../pages/meteo/meteo';
-import { OpenweathermapApiService } from '../services/openweathermapapi.service';
-import { SQLitePage } from '../pages/sqlite/sqlite';
-import { MapsPage } from '../pages/maps/maps';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import {DetailsPage} from '../pages/details/details';
+import {MeteoPage} from '../pages/meteo/meteo';
+import {OpenweathermapApiService} from '../services/openweathermapapi.service';
+import {SQLitePage} from '../pages/sqlite/sqlite';
+import {MapsPage} from '../pages/maps/maps';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { SQLite } from '@ionic-native/sqlite';
-import { GoogleMaps } from '@ionic-native/google-maps';
-import { Geolocation } from '@ionic-native/geolocation';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {SQLite} from '@ionic-native/sqlite';
+import {GoogleMaps} from '@ionic-native/google-maps';
+import {Geolocation} from '@ionic-native/geolocation';
 
-const cloudSettings: CloudSettings = {
+const cloudSettings : CloudSettings = {
   'core': {
-    'app_id': '7e409efd',
+    'app_id': '7e409efd'
   },
   'push': {
     'sender_id': '265179544171',
@@ -52,9 +52,7 @@ const cloudSettings: CloudSettings = {
     MapsPage
   ],
   imports: [
-    HttpModule,
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpModule, BrowserModule, IonicModule.forRoot(MyApp),
     CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
@@ -75,8 +73,10 @@ const cloudSettings: CloudSettings = {
     SplashScreen,
     SQLite,
     GoogleMaps,
-    Geolocation,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    Geolocation, {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler
+    }
   ]
 })
-export class AppModule { }
+export class AppModule {}
